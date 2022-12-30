416ebda0f015a33d3d5dac0de774b469d5e68f81
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2022-12-30 18:32:12 +0100

    added the gen_model for general attributes for other classes.

--- models/gen_model.py
+++ models/gen_model.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
-""" """
+""" Defines the base model for all classes. """
 from uuid import uuid4
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import models
+
+
+Base = declarative_base()
 
 
 class GenModel():
-    def __init__(self, *arg, **kwargs):
+    """
+    defines all common attributes for all other classes
+    attrs:
+        id:          identity string
+        created_at:  time created
+        updated_at:  time updated
+    """
+    id = Column(String(60), nullable=False, unique=True, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
+    def __init__(self, *args, **kwargs):
         if not kwargs:
             self.id = str(uuid4())
             self.created_at = datetime.utcnow()
@@ -22,3 +39,18 @@ class GenModel():
                 self.created_at = datetime.utcnow()
             if 'updated_at' not in kwargs:
                 self.updated_at = datetime.utcnow()
+
+    def save(self):
+        """ update the 'updated_at' time """
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
+
+    def delete(self):
+        """ deletes current instance from storage """
+        models.storage.delete(self)
+
+    def __str__(self):
+        """string representation of class """
+        return "[{:s}] with identity number: ({:s})\n{}\n{}".format(
+               self.__class__.__name__, self.id, '*' * 70, self.__dict__)
