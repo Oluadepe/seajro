1dcd5e3b31b80eb1040a54bbe8bd40258ec1e6f0
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2023-01-11 23:12:21 +0100

    optimized routes

--- routes.py
+++ routes.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ """
+from flask_login import LoginManager
 from flask import Flask, redirect, url_for, request, session, render_template
-from flask import flash
+from flask import flash, login_required
+from datetime import datetime
 from models.favourite import Favourite
 from models.feedback import Feedback
-from models.user import User
+from models.user import User, Event
 from models import storage
-from flask_wtf.csrf import csrf_protect
+#from flask_wtf.csrf import csrf_protect
 import hashlib
 import secrets
-
 
 
 app = Flask(__name__)
@@ -25,17 +26,29 @@ def close(exception):
 
 @app.route('/')
 def home():
-    return 'This os the Home Page!\nWeather data will be generated'
+    if session['logged_in'] is not True:
+        render_template('index.html')
+    else:
+        user = list(storage.retrieve(User, session['email']).values())[0]
+        events = []
+        for ev in user.events:
+            dic = {}
+            dic['title'] = ev.title
+            dic['date'] = ev.date
+            events.append(dic)
+
+        #
+        return render_template('index.html', events=events)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     """ sign up form """
     if request.method == 'GET':
-        return render_template('signup.html')
+        return render_template('register.html')
     email = request.form['email']
-    first_name = request.form['firstName']
-    last_name = request.form['lastName']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
     password = request.form['password']
     state = request.form['state']
     country = request.form['country']
@@ -43,15 +56,15 @@ def signup():
     if email:
         try:
             user = storage.retrieve(User, email).values()
-            if user:
-                if user is None:
+            if len(user) == 1:
+                if list(user)[0] is None:
                     raise Exception('Value of user is None')
                 else:
                     flash('User already exists.', 'info')
                     return redirect(url_for('signin'))
         except Exception:
             pass
-    user = User(email=email, first_name=first_name, last_name=last_name
+    user = User(email=email, first_name=first_name, last_name=last_name,
                 password=password, state=state, country=country, city=city)
     storage.new(user)
     storage.save()
@@ -67,8 +80,10 @@ def signin():
         password = request.form['password']
         password = hashlib.md5(password.encode()).hexdigest()
         user = storage.retrieve(User, email).values()
-        if user['password'] == password:
+        user = list(user)
+        if user[0]['password'] == password:
             session['logged_in'] = True
+            session['email'] = email
             flash('You have been logged in')
             return redirect(url_for('user'))
         else:
@@ -84,21 +99,35 @@ def signin():
 
 
 @app.route('/user')
-@csrf_protect
+#@csrf_protect
 @login_required
 def user():
     return 'User Landing Page/Dashboard'
 
 
-@app.route('/user/schedule')
-@csrf_protect
+@app.route('/user/schedule', methods=['POST'])
+#@csrf_protect
 @login_required
 def schedule():
-    return "returns schedule page where user can set schedules"
+    user_event = request.json
+    date = user_event['date']
+    date = datetime.strptime(date, '%Y-%m-%d').date()
+    title = user_event['title']
+    country = user_event['country']
+    city = user_event['city']
+    state = user_event['state']
+    user = storage.retrieve(User, session['email']).values()
+    user = list(user)[0]
+    event = Event(title=title, date=date, country=country, city=city,
+                  state=state, User=user)
+    storage.new(event)
+    storage.save()
+    flash('Successfully added an event!')
+    return redirect(url_for('home'))
 
 
 @app.route('/signout', methods=['GET'])
-@csrf_protect
+#@csrf_protect
 @login_required
 def signout():
     session.pop('logged_in', None)
