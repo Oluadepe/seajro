06d6e87478680bb72e93c0ae65cba173e1c5c06e
Ajiboye Adeleye <ajiboyeadeleye080@gmail.com>
2023-01-12 10:13:48 +0100

    added 'ondelete' and 'onupdate' to foreignKey

--- models/feedback.py
+++ models/feedback.py
@@ -7,7 +7,9 @@ from sqlalchemy import String, Column, ForeignKey
 class Feedback(GenModel, Base):
     """ storage for user feedback """
     __tablename__ = 'feedbacks'
-    user_email = Column(String(60), ForeignKey('users.email'), nullable=False)
+    user_email = Column(String(60),
+                        ForeignKey('users.email', onupdate="CASCADE",
+                                   ondelete="CASCADE"), nullable=False)
     city = Column(String(60), nullable=False)
     state = Column(String(60), nullable=False)
     country = Column(String(60), nullable=False)
